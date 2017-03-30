788392613bb5e0be7900f9b4468d12a872b4c472
Oswaldo Almazo <oswaldo.almazo@gmail.com>
2017-03-30 16:58:22 -0400

    greet new user and alert others

--- server/server.js
+++ server/server.js
@@ -13,6 +13,18 @@ app.use(express.static(publicPath));
 
 io.on('connection', (socket) =>{
     console.log('new user connected');
+    
+    socket.emit('newMessage', {
+        from: 'Admin',
+        text: 'Welcome to the Chat App!',
+        createdAt: new Date().getTime()
+    });
+
+    socket.broadcast.emit('newMessage', {
+        from: 'Admin',
+        text: 'New user joined!',
+        createdAt: new Date().getTime()
+    });
 
     socket.on('createMessage', (message) => {
         console.log('new message', message);
@@ -21,6 +33,11 @@ io.on('connection', (socket) =>{
             text: message.text,
             createdAt: new Date().getTime()
         });
+        // socket.broadcast.emit('newMessage', {
+        //     from: message.from,
+        //     text: message.text,
+        //     createdAt: new Date().getTime()
+        // });
     });
     socket.on('disconnect', (socket) => {
         console.log('user diconnected');
