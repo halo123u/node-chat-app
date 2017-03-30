8abb29bd60180b29004712b27c00598895a51bd6
Oswaldo Almazo <oswaldo.almazo@gmail.com>
2017-03-30 16:11:11 -0400

    emmit newMessage on createMessage

--- public/js/index.js
+++ public/js/index.js
@@ -1,10 +1,6 @@
 var socket = io();
 socket.on('connect', function () {
     console.log('connected to server');
-   socket.emit('createMessage', {
-       from: 'User2',
-       text: 'im doint well'
-   })
 });
 
 socket.on('newMessage', function (message){
--- server/server.js
+++ server/server.js
@@ -14,14 +14,13 @@ app.use(express.static(publicPath));
 io.on('connection', (socket) =>{
     console.log('new user connected');
 
-    socket.emit('newMessage', {
-        from: 'User1',
-        text: 'whats up',
-        createdAt: 123
-    });
-
     socket.on('createMessage', (message) => {
         console.log('new message', message);
+        io.emit('newMessage', {
+            from: message.from,
+            text: message.text,
+            createdAt: new Date().getTime()
+        });
     });
     socket.on('disconnect', (socket) => {
         console.log('user diconnected');
